eb7ab9631a8e4e6f83c9eb691a22fccb60c22962
pelin engin duran <pelinenginduran@pelin-MacBook-Air.local>
2024-06-08 17:51:59 +0300

    initilaze project

---
+++ App.js
@@ -0,0 +1,40 @@
+
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+function App(){
+  return (
+    <SafeAreaView>
+    <View>
+    <Text>TODOAPP</Text>
+    </View>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
