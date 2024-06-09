75d33e1864ab75ed113a98974d63ecbf24b335f8
pelin engin duran <pelinenginduran@pelin-MacBook-Air.local>
2024-06-09 22:17:35 +0300

    add icon

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -12,21 +11,18 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import Header from './src/components/header';
+import generalStyles from './src/utils/generalStyle';
+import Icon from "react-native-vector-icons/dist/AntDesign"
+import { colors } from './src/utils/constants';
 
 function App(){
   return (
-    <SafeAreaView>
+    <SafeAreaView style={generalStyles.flex1}>
+      <Header title="My To Do App"/>
     <View>
     <Text>TODOAPP</Text>
+    <Icon name="shrink" size={25} color={colors.bgPrimary}/>
     </View>
 
     </SafeAreaView>
@@ -36,5 +32,6 @@ function App(){
 const styles = StyleSheet.create({
   
 });
+
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const colors={
+    bgPrimary:"#E88D67",
+    textPrimary:"#365E32",
+    textSecondry:"#1A2130"
+
+}
