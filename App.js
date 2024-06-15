82dac9a44f90a7397757d4e5518bf71601e25e33
Pelin Engin Duran <143960111+PelinEnginDuran@users.noreply.github.com>
2024-06-15 18:26:21 +0300

    Revert "new"

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
 
-import React,{useState} from 'react';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -9,61 +10,31 @@ import {
   Text,
   useColorScheme,
   View,
-  Alert
 } from 'react-native';
 
-import Header from './src/components/header';
-import generalStyles from './src/utils/generalStyle';
-import Icon from "react-native-vector-icons/AntDesign"
-import { colors } from './src/utils/constants';
-import Input from "./src/components/input";
-import styles from './src/components/header/style';
-import Todo from './src/components/todo';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 
 
 function App(){
-  const[text,setText]=useState("")
-  const[todos,setTodos]=useState([])
-  const addTodo=()=>{
-    const newTodo={
-      id: String(new Date().getTime()),
-      text:text,
-      date: new Date(),
-      completed: false
-    }
-    setTodos([...todos,newTodo])
-    setText("")
-  }
   return (
-    <SafeAreaView style={[generalStyles.flex1, generalStyles.bgWhite]}>
-      <Header title="My To Do App" />
-      <Input 
-      value={text}
-      onChangeText={(text)=>setText(text)}
-      placeholder='birşeyler yaz' 
-      hasIcon
-      IconName="arrowleft" 
-      onIconPress={addTodo}
-      />
-      <View style={generalStyles.todosWrapper}>
-        {todos.length === 0  ? (
-        <Text style={generalStyles.emptyText}>henüz kayıtlı to do bulunamadı...</Text>
-      ) : (
-           <ScrollView style={styles.ScrollView}>
-             
-             { todos?.map((todo)=>(<Todo key={todo?.id} todo={todo}/>))}
-            
-           </ScrollView>
-          )}
+    <SafeAreaView>
+    <View>
+    <Text>TODOAPP</Text>
+    </View>
 
-      </View>
-          
     </SafeAreaView>
   );
 }
 
-
-
+const styles = StyleSheet.create({
+  
+});
 
 export default App;
